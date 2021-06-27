27f88c0a3a7117fdcdda889537e23e1d5d068d47
Hung <hunghk.it@gmail.com>
2021-06-27 18:21:11 +0700

    push build

---
+++ dist/constants.d.ts
@@ -0,0 +1,35 @@
+import JSBI from 'jsbi';
+export declare type BigintIsh = JSBI | bigint | string;
+export declare enum ChainId {
+    MAINNET = 56,
+    BSCTESTNET = 97
+}
+export declare enum TradeType {
+    EXACT_INPUT = 0,
+    EXACT_OUTPUT = 1
+}
+export declare enum Rounding {
+    ROUND_DOWN = 0,
+    ROUND_HALF_UP = 1,
+    ROUND_UP = 2
+}
+export declare const FACTORY_ADDRESS = "0x002A4C02e4A0a2a130d56FbE20eA6923345f270D";
+export declare const INIT_CODE_HASH = "0xb053e979e8253361b93aa7548e9da09c17538a39090546d995983cc89544de1e";
+export declare const MINIMUM_LIQUIDITY: JSBI;
+export declare const ZERO: JSBI;
+export declare const ONE: JSBI;
+export declare const TWO: JSBI;
+export declare const THREE: JSBI;
+export declare const FIVE: JSBI;
+export declare const TEN: JSBI;
+export declare const _100: JSBI;
+export declare const _998: JSBI;
+export declare const _1000: JSBI;
+export declare enum SolidityType {
+    uint8 = "uint8",
+    uint256 = "uint256"
+}
+export declare const SOLIDITY_TYPE_MAXIMA: {
+    uint8: JSBI;
+    uint256: JSBI;
+};
